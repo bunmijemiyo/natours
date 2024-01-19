cd422185c80cc7920d9097deae75107c2b6bb67e
olubunmi <bjemiyor@gmail.com>
2024-01-19 12:32:00 +0100

    Improved Stripe implementation 2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -39,7 +39,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+            // images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${
+                tour.imageCover
+              }`,
+            ],
           },
           unit_amount: tour.price * 100,
         },
@@ -89,7 +94,7 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`webhook error: ${err.message}`);
   }
 
-  if (event.type === `checkout.session.complete`) {
+  if (event.type === `checkout.session.completed`) {
     createBookingCheckout(event.data.object);
   }
   res.status(200).json({ received: true });
